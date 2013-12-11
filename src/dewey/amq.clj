b61e1d927c142ec9047c79351a80a289f83818aa
Tony Edgin <tedgin@iplantcollaborative.org>
2013-12-11 13:50:08 -0700

    refactoring
    The configuration values for the AMQP queue connection have been moved to core.clj.
    
    The callback function is now provided by a factory function.

--- src/dewey/amq.clj
+++ src/dewey/amq.clj
@@ -1,4 +1,5 @@
 (ns dewey.amq
+  "This library mananges the connection to the AMQP queue."
   (:require [cheshire.core :as json]
             [langohr.channel :as lch]
             [langohr.core :as rmq]
@@ -6,18 +7,32 @@
             [langohr.exchange :as le]
             [langohr.queue :as lq]))
 
-(def ^{:const true :private true} exchange "irods")
+
+(defn- mk-handler
+  [consume]
+  (fn [_ {:keys [routing-key]} ^bytes payload]
+    (consume routing-key (json/parse-string (String. payload "UTF-8") true))))
+
 
 (defn attach-to-exchange
-  [queue consume & topics]
+  "Attaches a consumer function to a given queue on a given AMQP exchange. It listens only for the
+   provided topics. If no topics are provided, it listens for all topics. It is assumed that the
+   messages in the queue are JSON documents.
+
+   Parameters:
+     exchange - the name of the exchange
+     queue - the name of the queue
+     consumer - the function that will receive the JSON document
+     topics - Optionally, a list of topics to listen for
+
+   TODO handle errors"
+  [exchange queue consumer & topics]
   (let [conn    (rmq/connect)
-        ch      (lch/open conn)
-        handler (fn [_ {:keys [routing-key]} ^bytes payload]
-                  (consume routing-key (json/parse-string (String. payload "UTF-8") true)))]
+        ch      (lch/open conn)]
     (le/declare ch exchange "topic")
     (lq/declare ch queue)
     (if (empty? topics)
       (lq/bind ch queue exchange :routing-key "#")
       (doseq [topic topics]
         (lq/bind ch queue exchange :routing-key topic)))
-    (lc/subscribe ch queue handler :auto-ack true)))
+    (lc/subscribe ch queue (mk-handler consumer) :auto-ack true)))
--- src/dewey/core.clj
+++ src/dewey/core.clj
@@ -5,6 +5,9 @@
             [dewey.amq :as amq]
             [dewey.indexing :as indexing]))
 
+(def ^{:const true :private true} exchange "irods")
+(def ^{:const true :private true} queue "indexing")
+
 (defn- init-irods
   []
   (let [cfg (r-init/init "localhost" "1247" "rods" "rods" "/iplant/home/rods" "iplant" "demoResc")]
@@ -15,4 +18,8 @@
 (defn -main
   [& args]
   (let [irods-cfg (init-irods)]
-    (amq/attach-to-exchange "indexing" (partial indexing/consume-msg irods-cfg))))
+    (amq/attach-to-exchange exchange
+                            queue
+                            (partial indexing/consume-msg irods-cfg)
+                            "data-object.#"
+                            "collection.#")))
