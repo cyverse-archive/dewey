2751f8015db2123ea2b63c517a992d9cfeb5ceb7
Tony Edgin <tedgin@iplantcollaborative.org>
2013-12-12 16:18:08 -0700

    updating rpm dependencies

--- project.clj
+++ project.clj
@@ -19,7 +19,7 @@
                    :resource-paths ["dev-resource"]}}
   :plugins [[org.iplantc/lein-iplant-rpm "1.4.3-SNAPSHOT"]]
   :iplant-rpm {:summary      "dewey"
-               :dependencies ["iplant-services-config >= 0.1.0-5"]
+               :dependencies ["iplant-service-config >= 0.1.0-5" "iplant-clavin"]
                :config-files ["log4j.properties"]
                :config-path  "resources"}
   :repositories {"iplantCollaborative"
