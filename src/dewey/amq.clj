fb30a4633c261ce0c0d9b643047fef0695058a22
Tony Edgin <tedgin@iplantcollaborative.org>
2014-01-21 17:08:14 -0700

    Added registration and cancellation handlers
    When a queue is deleted or a consumer is cancel, the channel will be set up again after a small delay.

--- src/dewey/amq.clj
+++ src/dewey/amq.clj
@@ -3,6 +3,7 @@
   (:use [slingshot.slingshot :only [try+]])
   (:require [clojure.tools.logging :as log]
             [cheshire.core :as json]
+            [langohr.basic :as lb]
             [langohr.channel :as lch]
             [langohr.core :as rmq]
             [langohr.consumers :as lc]
@@ -19,6 +20,27 @@
         (log/error (:throwable &throw-context) "MESSAGE HANDLING ERROR")))))
 
 
+(defn- consume
+  [connection exchange-name exchange-durable exchange-auto-delete topics delivery-fn]
+  (let [channel  (lch/open connection)
+        queue    "indexing"
+        consumer (lc/create-default channel
+                   :handle-consume-ok-fn (fn [_] (log/info "Registered with AMQP broker"))
+                   :handle-delivery-fn   delivery-fn
+                   :handle-cancel-fn     (fn [_] (log/info "AMQP broker registration canceled")
+                                                 (Thread/sleep 1000)
+                                                 (consume connection
+                                                          exchange-name
+                                                          exchange-durable
+                                                          exchange-auto-delete
+                                                          topics
+                                                          delivery-fn)))]
+    (le/topic channel exchange-name :durable exchange-durable :auto-delete exchange-auto-delete)
+    (lq/declare channel queue :durable true)
+    (doseq [topic topics] (lq/bind channel queue exchange-name :routing-key topic))
+    (lb/consume channel queue consumer :auto-ack true)))
+
+
 (defn attach-to-exchange
   "Attaches a consumer function to a given queue on a given AMQP exchange. It listens only for the
    provided topics. If no topics are provided, it listens for all topics. It is assumed that the
@@ -33,20 +55,16 @@
      exchange-durable     - a flag indicating whether or not the exchange preserves messages
      exchange-auto-delete - a flag indicating whether or not the exchange is deleted when all queues
                             have been dettached
-     consumer             - the function that will receive the JSON document
+     consumer-fn          - the function that will receive the JSON document
      topics               - Optionally, a list of topics to listen for
 
    Throws:
      It will throw an exception if it fails to connect to the AMQP broker, setup the exchange, or
      setup the queue."
-  [host port user password exchange-name exchange-durable exchange-auto-delete consumer & topics]
-  (let [conn  (rmq/connect {:host host :port port :username user :password password})
-        ch    (lch/open conn)
-        queue "indexing"]
-    (le/topic ch exchange-name :durable exchange-durable :auto-delete exchange-auto-delete)
-    (lq/declare ch queue :durable true)
-    (if (empty? topics)
-      (lq/bind ch queue exchange-name :routing-key "#")
-      (doseq [topic topics]
-        (lq/bind ch queue exchange-name :routing-key topic)))
-    (lc/subscribe ch queue (mk-handler consumer) :auto-ack true)))
+  [host port user password exchange-name exchange-durable exchange-auto-delete consumer-fn & topics]
+  (consume (rmq/connect {:host host :port port :username user :password password})
+           exchange-name
+           exchange-durable
+           exchange-auto-delete
+           (if (empty? topics) "#" topics)
+           (mk-handler consumer-fn)))
